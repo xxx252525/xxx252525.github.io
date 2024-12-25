2589660c71f2018269eab239220251f9eada5a84
xxx252525 <1072441436@qq.com>
2024-12-25 20:07:17 +0800

    文章更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,3 +6,20 @@ export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
+
+module.exports = {
+  title: '天阁创客official的书屋',
+  description: '涵盖诸多教程',
+      themeConfig: {
+          nav: [
+              { text: '首页', link: '/' },
+              { 
+                  text: '天阁创客official的书屋', 
+                  items: [
+                      { text: 'Github', link: 'https://github.com/xxx252525' },
+                      { text: '哔哩哔哩', link: 'https://space.bilibili.com/617633317/?spm_id_from=333.999.0.0' }
+                  ]
+              }
+          ]
+      }
+}
