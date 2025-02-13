fe86b0d2be9826654c01b6be6b57064be461ccc8
xxx252525 <1072441436@qq.com>
2025-02-13 17:58:14 +0800

    文章排列顺序修复

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,21 +1,21 @@
 import { sidebar } from "vuepress-theme-hope";
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
-function getSortedChildren(prefix) {
-  const dirPath = path.resolve(__dirname, '../', prefix);
-  const files = fs.readdirSync(dirPath);
+// function getSortedChildren(prefix) {
+//   const dirPath = path.resolve(__dirname, '../', prefix);
+//   const files = fs.readdirSync(dirPath);
   
-  // 按照文件名升序排序
-  const sortedFiles = files
-    .filter(file => file.endsWith('.md'))
-    .sort((a, b) => a.localeCompare(b));
+//   // 按照文件名升序排序
+//   const sortedFiles = files
+//     .filter(file => file.endsWith('.md'))
+//     .sort((a, b) => a.localeCompare(b));
   
-  return sortedFiles.map(file => ({
-    text: file.replace('.md', ''),
-    link: `/${prefix}${file}`,
-  }));
-}
+//   return sortedFiles.map(file => ({
+//     text: file.replace('.md', ''),
+//     link: `/${prefix}${file}`,
+//   }));
+// }
 
 
 export default sidebar({
